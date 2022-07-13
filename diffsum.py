7a80d43dbbfdc81622d078fae5fa4b08ce5544dd
tanupoo <github-shoichi@tanu.org>
2022-07-13 14:20:19 +0900

    renamed to diffsum.py

--- diffsum.py
+++ diffsum.py
@@ -1,20 +1,18 @@
 from datetime import datetime, timedelta
 
-def diffsumspan(data, sumspan=300, margin=10, offset=0):
+def diffsum_timespan_sharp(data, sumspan=300, margin=10):
     """
     data: [ (scire(timestamp), value, isoformat), ... ]
     sumspan: span in seconds to integrate the diff values.
     margin: margin in seconds, which allows the end of the integration.
-        sumspan丁度にタイムスタンプが収まらないので余裕をもたせる。
-    offset: the offset of the list in data to start the integration.
     return: [ value1, ... ]
         if there is no enough data for the integration, the sum value sets None.
     """
     vsum = []
-    ts0 = data[offset][0]
+    ts0 = data[0][0]
     vs = 0
     good = False
-    for i in range(offset+1,len(data)):
+    for i in range(1,len(data)):
         di = data[i]
         print("delta:", data[i][0] - ts0)
         if data[i][0] - ts0 < sumspan + margin:
