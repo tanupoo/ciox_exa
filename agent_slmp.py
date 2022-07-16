9c741044e13bc575797ea9c00e1ef8e070841ba0
tanupoo <github-shoichi@tanu.org>
2022-07-17 00:32:37 +0900

    removed to add pythonpath.

--- agent_slmp.py
+++ agent_slmp.py
@@ -1,6 +1,3 @@
-import sys
-sys.path.insert(0, sys.path[0]+"/PySLMPClient") # XXX ugly
-
 from pyslmpclient import SLMPClient
 from pyslmpclient.const import DeviceCode
 from pyslmpclient.util import Target
