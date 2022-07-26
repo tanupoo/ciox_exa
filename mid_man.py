e8f4ed1cb041777500e7bf5ea70bf38b14aeb4ea
tanupoo <github-shoichi@tanu.org>
2022-07-26 21:58:58 +0900

    improved error handling when a connection failure to the db happened.

--- mid_man.py
+++ mid_man.py
@@ -14,38 +14,63 @@ def worker(server, config):
     db_que = DBConnector(config.QueDB, config)
     tag = f"Mid:{server.ServerID}: "
     while True:
+        # since db is set to None when the connection has been rejected.
+        # see below.
+        if db_agent is None:
+            db_agent = DBConnector(config.AgentDB, config)
+        if db_que is None:
+            db_que = DBConnector(config.QueDB, config)
+        #
         config.logger.debug(f"{tag}resumed: {get_time_isoformat()}")
         cur_ts = get_time_now().timestamp()
         # IntegrationDeferTime delays to start this process.
         # So, cur_ts is enough to get the number of sampling data.
-        psvt = db_agent.get_data_raw(server.Points, cur_ts)
-        out_data = []
-        for pid,svt in psvt.items():
-            config.logger.debug(f"{tag}{pid}: {len(svt)} data exist.")
-            if len(svt) > 0:
-                if config.Points[pid].Integration is True:
-                    sumspan = config.Points[pid].IntegrationSpan
-                    ret = diffsum_timespan_sharp(
-                            svt, sumspan=sumspan,
-                            margin=server.IntegrationMarginTime,
-                            config=config)
-                    vsum_list = ret["vsum_list"]
-                    rest_offset = ret["rest_offset"]
-                    vx = PointDataWithTS(PointID=pid, Values=vsum_list,
-                                         TSLastValue=get_time_isoformat(ts=svt[rest_offset][0]))
-                    config.logger.debug(f"{tag}{pid}: {vx.Values} {vx.TSLastValue}")
-                    out_data.append(vx.dict())
-                    if rest_offset > 0:
-                        # Note: score - 1 is to leave the latest data.
-                        last_score = svt[rest_offset-1][0] - 1
-                        db_agent.del_data_raw(server.Points, last_score)
-                else:
-                    raise NotImplemented(f"{tag}{pid} Integration False is not supported yet.")
+        try:
+            psvt = db_agent.get_data_raw(server.Points, cur_ts)
+        except db_agent.ConnectionError as e:
+            db_agent = None
+            # through here internal loop and try to connect DB.
+        else:
+            out_data = []
+            del_data = []
+            for pid,svt in psvt.items():
+                config.logger.debug(f"{tag}{pid}: {len(svt)} data exist.")
+                if len(svt) > 0:
+                    if config.Points[pid].Integration is True:
+                        sumspan = config.Points[pid].IntegrationSpan
+                        ret = diffsum_timespan_sharp(
+                                svt, sumspan=sumspan,
+                                margin=server.IntegrationMarginTime,
+                                config=config)
+                        vsum_list = ret["vsum_list"]
+                        rest_offset = ret["rest_offset"]
+                        vx = PointDataWithTS(PointID=pid, Values=vsum_list,
+                                            TSLastValue=get_time_isoformat(ts=svt[rest_offset][0]))
+                        config.logger.debug(f"{tag}{pid}: {vx.Values} {vx.TSLastValue}")
+                        out_data.append(vx.dict())
+                        if rest_offset > 0:
+                            # Note: score - 1 is to leave the latest data.
+                            last_score = svt[rest_offset-1][0] - 1
+                            del_data.append((server.Points, last_score))
 
-        if len(out_data) > 0:
-            config.logger.debug(f"{tag}{len(out_data)} data queued.")
-            config.logger.debug(f"{tag}{out_data}")
-            db_que.post_data_ready(server.ServerID, out_data)
+            # post data to the queue db.
+            if len(out_data) > 0:
+                config.logger.debug(f"{tag}{len(out_data)} data queued.")
+                config.logger.debug(f"{tag}{out_data}")
+                try:
+                    db_que.post_data_ready(server.ServerID, out_data)
+                except db_que.ConnectionError as e:
+                    db_que = None
+                    # try to connect DB.
+                else:
+                    # delete data if posting data is successful.
+                    try:
+                        for p,s in del_data:
+                            db_agent.del_data_raw(p,s)
+                    except db_agent.ConnectionError as e:
+                        db_agent = None
+                        # through here and try to connedt to the db next time.
+                        pass
         # sleep
         config.logger.debug(f"{tag}sleep: {get_time_isoformat()}")
         time.sleep(get_sleep_time(server.PostInterval,
