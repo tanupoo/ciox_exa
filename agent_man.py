47f10c4019a0cda30c275f41d566de8855c54c2f
tanupoo <github-shoichi@tanu.org>
2022-07-26 19:33:56 +0900

    enabled to reconnect to the redis db if the connection was filed.

--- agent_man.py
+++ agent_man.py
@@ -8,10 +8,20 @@ import time
 import sys
 from time_util import get_sleep_time, get_time_isoformat
 
-def agent_worker(agent):
-    db = DBConnector(config.AgentDB, config)
-    tag = f"Agent:{agent.target.TargetID}: "
+def agent_worker(target, config):
+    # make an agent.
+    if target.Protocol == "SLMP":
+        agent = slmp_start_agent(target, config)
+    else:
+        raise ValueError("unknown Protocol {x.Protocol}.")
+    # start the main loop
+    db = None
     while True:
+        tag = f"Agent:{agent.target.TargetID}: "
+        if db is None:
+            # since db is set to None when the connection has been rejected.
+            # see below.
+            db = DBConnector(config.AgentDB, config)
         agent.logger.debug(f"{tag}resumed: {get_time_isoformat()}")
         try:
             result = agent.pull()
@@ -28,22 +38,25 @@ def agent_worker(agent):
             pdata = []
             for p in agent.target.Points:
                 pdata.append(( p.PointID, result[p.Position-1] ))
-            db.post_data_raw(pdata)
-            agent.logger.debug(f"{tag}posted data: {pdata}")
+            try:
+                db.post_data_raw(pdata)
+            except db.ConnectionError as e:
+                db = None
+                # through here internal loop and try to connect DB.
+            else:
+                agent.logger.debug(f"{tag}posted data: {pdata}")
         agent.logger.debug(f"{tag}sleep: {get_time_isoformat()}")
         time.sleep(get_sleep_time(agent.PullInterval))
 
 def start_wokers(config):
     workers = []
     for x in config.Targets:
-        config.logger.debug(f"starting worker for {x.TargetID}")
-        if x.Protocol == "SLMP":
-            agent = slmp_start_agent(x, config)
-            workers.append(threading.Thread(target=agent_worker,
-                                            args=(agent,),
-                                            daemon=False))
-        else:
-            raise ValueError("unknown Protocol {x.Protocol}.")
+        agent = threading.Thread(target=agent_worker,
+                                 args=(x, config,),
+                                 daemon=False)
+        if agent is not False:
+            workers.append(agent)
+            config.logger.debug(f"starting worker for {x.TargetID}")
     # start threads.
     for x in workers:
         x.start()
