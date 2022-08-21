e66479993f661f638d9b37c46344cb567dd6082f
tanupoo <github-shoichi@tanu.org>
2022-08-21 18:00:48 +0900

    added to support ulimit.

--- diffsum.py
+++ diffsum.py
@@ -1,10 +1,15 @@
 from time_util import get_sharp_timestamp, get_time_isoformat
 
-def diffsum_timespan_sharp(data, sumspan=300, margin=10, config=None):
+def diffsum_timespan_sharp(data, sumspan=300, margin=10,
+                           ulimit=None, config=None):
     """
     data: [ (score(timestamp), value, isoformat), ... ]
     sumspan: span in seconds to integrate the diff values.
     margin: margin in seconds, which allows the delay of sampleing timestamp.
+    ulimit: upper limit is not None, and
+        if a data is more than ulimit, the data is ignored.
+        if the data A is bigger than the next one B, diff woul be:
+            diff = 1 + ulimit - A + B
 
     return: { "vsum_list": [ value1, ... ], "rest_offset": rest_data_offset }
         rest_data_offset := None: no data remained.
@@ -45,22 +50,36 @@ Value        v.   v0    v1    v2    v3    v4    v5    v6    v7
     e.g. if the set of data is [t1, t2, t3, t4, t5, t6, t7]
     as st1 is the sharp time, so the start is t3, offset is 2.
     """
+    def get_diff_ulimit(v0, v1, ulimit=0):
+        if v0 > v1:
+            return 1 + ulimit - v0 + v1
+        else:
+            return v1 - v0
+    #
+    def get_diff_simple(v0, v1, ulimit=0):
+        return v1 - v0
+    #
+    if ulimit is not None:
+        get_diff = get_diff_ulimit
+    else:
+        get_diff = get_diff_simple
+    #
     vsum_list = []
     vsum = 0
-    v0 = data[offset][1]
+    v0 = float(data[offset][1])
     offset_completed = 0
     for offset in range(offset+1,data_size):
         (s,v,t) = data[offset]
+        v = float(v)
         diff = s - get_sharp_timestamp(sumspan, s, prev=True)
+        vsum += get_diff(v0, v, ulimit)
         if diff <= margin:
             debug_print(f"Z {offset} {get_time_isoformat(ts=s)} {diff}")
-            vsum += (float(v) - float(v0))
             vsum_list.append(vsum)
             vsum = 0
             offset_completed = offset
         else:
             debug_print(f"* {offset} {get_time_isoformat(ts=s)} {diff}")
-            vsum += (float(v) - float(v0))
         v0 = v
     result["vsum_list"] = vsum_list
     result["rest_offset"] = offset_completed
