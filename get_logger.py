ff48613193560d8a9034796011df8b4295456556
tanupoo <github-shoichi@tanu.org>
2022-08-22 19:18:08 +0900

    added usage.

--- get_logger.py
+++ get_logger.py
@@ -5,6 +5,11 @@ LOG_FORMAT = "%(asctime)s.%(msecs)d %(lineno)d %(levelname)s %(message)s"
 LOG_DATE_FORMAT = "%Y-%m-%dT%H:%M:%S"
 
 def get_logger(prog_name=None, log_file=None, debug_mode=False, syslog=None):
+    # log_file: a file name for logging.
+    #           `-` means to show log onto stdout.
+    #           `syslog` means to use syslog.
+    # debug_mode: set log level to DEBUG, otherwise set to INFO.
+    # syslog: syslog server's address
     def get_logging_handler(channel):
         channel.setFormatter(logging.Formatter(fmt=LOG_FORMAT,
                                                datefmt=LOG_DATE_FORMAT))
@@ -14,10 +19,6 @@ def get_logger(prog_name=None, log_file=None, debug_mode=False, syslog=None):
             channel.setLevel(logging.INFO)
         return channel
 
-    #
-    # set logger.
-    #   log_file: a file name for logging.
-    #   `-` means to show log onto stdout.
     logger = logging.getLogger(prog_name)
     if log_file == "-":
         logger.addHandler(get_logging_handler(logging.StreamHandler()))
